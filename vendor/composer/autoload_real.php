1938c3bf1143acd6dcde1786742095babfd0bd19
linhfishCR7 <59117535+linhfishCR7@users.noreply.github.com>
2020-12-04 19:43:59 +0700

    product table

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,13 +22,15 @@ class ComposerAutoloaderInit80b36ba914d58aa257da4443b3a714be
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit80b36ba914d58aa257da4443b3a714be', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit80b36ba914d58aa257da4443b3a714be', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInit80b36ba914d58aa257da4443b3a714be::getInitializer($loader));
         } else {
